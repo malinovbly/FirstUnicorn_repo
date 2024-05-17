c047f3591150791d550b6fef4e705abe0f16029a
Artemy <temanov0530@gmail.com>
2024-05-17 23:47:21 +0500

    Добавлена ссылка на страницу с вакансиями. Более подробно рассмотрены возможности context в views, а также цикл for в разметке.

--- vacancies/views.py
+++ vacancies/views.py
@@ -3,7 +3,19 @@ from django.shortcuts import render
 
 def vacancies(request):
     context = {
-         'title': 'FirstUnicorn | Вакансии'
+        'title': 'FirstUnicorn | Вакансии',
+        'vacancies': [
+            {
+                'name': 'Производство втулок.',
+                'description': 'Изготовление втулок на станке. Срок - 4 дня.',
+                'price': '500.00',
+            },
+            {
+                'name': 'Замена подшипника.',
+                'description': 'Произведу замену старого подшипника в детали. Срок - 7 дней.',
+                'price': '700.00',
+            },
+        ]
     }
 
     return render(request, 'vacancies/vacancies.html', context)
