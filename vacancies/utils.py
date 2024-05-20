54e2523be7a65308cb598b7304c92d10f39612dd
Artemy <temanov0530@gmail.com>
2024-05-20 20:27:58 +0500

    Полностью готов полнотекстовый поиск, но с одной недоработкой (рещил на нее забить)

--- vacancies/utils.py
+++ vacancies/utils.py
@@ -1,5 +1,5 @@
 from django.db.models import Q
-from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank, SearchHeadline
 from vacancies.models import Vacancy
 
 
@@ -7,11 +7,31 @@ def q_search(query):
 
     if query.isdigit() and len(query) <= 5:
         return Vacancy.objects.filter(id=int(query))
-    
+
     vector = SearchVector("name", "description")
     query = SearchQuery(query)
 
-    return Vacancy.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
+    result = Vacancy.objects.annotate(rank=SearchRank(vector, query)).filter(rank__gt=0).order_by("-rank")
+
+    result = result.annotate(
+        headline=SearchHeadline(
+            "name",
+            query,
+            start_sel='<span style="background-color: orange;">',
+            stop_sel='</span>'
+        )
+    )
+
+    result = result.annotate(
+        bodyline=SearchHeadline(
+            "description",
+            query,
+            start_sel='<span style="background-color: orange;">',
+            stop_sel='</span>'
+        )
+    )
+
+    return result
 
 
     # keywords = [word for word in query.split() if len(word) > 2]
@@ -22,4 +42,3 @@ def q_search(query):
     #     q_objects |= Q(name__icontains=token)
 
     # return Vacancy.objects.filter(q_objects)
-    
