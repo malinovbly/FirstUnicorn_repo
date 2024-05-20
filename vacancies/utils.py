7e837ff655e8b0be1b1da6aed78c137ba22bbe05
Artemy <temanov0530@gmail.com>
2024-05-20 19:18:47 +0500

    Подключена СУБД PostgreSQL, готов полнотекстовый поиск

--- vacancies/utils.py
+++ vacancies/utils.py
@@ -1,4 +1,5 @@
 from django.db.models import Q
+from django.contrib.postgres.search import SearchVector, SearchQuery, SearchRank
 from vacancies.models import Vacancy
 
 
@@ -7,12 +8,18 @@ def q_search(query):
     if query.isdigit() and len(query) <= 5:
         return Vacancy.objects.filter(id=int(query))
     
-    keywords = [word for word in query.split() if len(word) > 2]
-    q_objects = Q()
+    vector = SearchVector("name", "description")
+    query = SearchQuery(query)
 
-    for token in keywords:
-        q_objects |= Q(description__icontains=token)
-        q_objects |= Q(name__icontains=token)
+    return Vacancy.objects.annotate(rank=SearchRank(vector, query)).order_by("-rank")
 
-    return Vacancy.objects.filter(q_objects)
+
+    # keywords = [word for word in query.split() if len(word) > 2]
+    # q_objects = Q()
+
+    # for token in keywords:
+    #     q_objects |= Q(description__icontains=token)
+    #     q_objects |= Q(name__icontains=token)
+
+    # return Vacancy.objects.filter(q_objects)
     
