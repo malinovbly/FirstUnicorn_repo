0d806984c7ac094c50b51b56ea30bb5d11ab3968
Artemy <temanov0530@gmail.com>
2024-05-21 23:40:48 +0500

    Регистрация готова

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,12 @@
 from django import forms
-from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from users.models import User
 
 
 class UserLoginForm(AuthenticationForm):
+
+    class Meta:
+        model = User
     
     username = forms.CharField(
         widget=forms.TextInput(attrs={
@@ -21,5 +24,31 @@ class UserLoginForm(AuthenticationForm):
         })
     )
 
+
+
+class UserRegistrationForm(UserCreationForm):
+
     class Meta:
         model = User
+        fields = (
+            "first_name",
+            "last_name",
+            "patronymic",
+            "username",
+            "email",
+            "password1",
+            "password2",
+            "city",
+            "profession",
+            "work_experience",
+        )
+
+    first_name = forms.CharField()
+    last_name = forms.CharField()
+    username = forms.CharField()
+    email = forms.CharField()
+    password1 = forms.CharField()
+    password2 = forms.CharField()
+    city = forms.CharField()
+    profession = forms.CharField()
+    work_experience = forms.CharField()
